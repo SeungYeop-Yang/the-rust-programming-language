0004ec3a11532635495c6eec49230b2f4d646ba1
SeungYeop Yang <seungyeop.yang@gmail.com>
2023-01-17 08:18:18 +0000

    chap04 continued

--- chap04/projects/ownership/src/main.rs
+++ chap04/projects/ownership/src/main.rs
@@ -1,17 +1,12 @@
 fn main() {
     // les s = "hello";                     // this is a literal
-    let s = String::from("hello");      // this is a variable; s is valid from this point forward
-    takes_ownership(s);                 // the scope of s is now over, and s is no longer valid
+    let mut s = String::from("hello");      // this is a variable; s is valid from this point forward
 
-    let x = 5;
-    makes_copy(x);
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r1}, {r2}");
 
-}
+    let r3 = &mut s;
 
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
-}   // the scope of some_string is now over, and some_string is no longer valid
-
-fn makes_copy(some_integer: i32) {
-    println!("{some_integer}");
+    println!("{r3}");
 }
